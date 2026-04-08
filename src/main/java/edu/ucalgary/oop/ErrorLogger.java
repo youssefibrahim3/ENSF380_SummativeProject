2b594b9bfb4ddacd2284d3f69c5f679cf7b46285
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-04-08 16:01:30 -0600

    added ErrorLogger

---
+++ src/main/java/edu/ucalgary/oop/ErrorLogger.java
@@ -0,0 +1,30 @@
+package edu.ucalgary.oop;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * Class for logging errors to data/errorlog.txt
+ */
+public class ErrorLogger {
+    private static final String FILE_NAME = "data/errorlog.txt";
+
+    /**
+     * Writes an error log as text into action_log.txt.
+     * The error message follows a specific format.
+     * 
+     * @param exception The Exception that the error caused
+     */
+    public static void log(Exception exception)
+    {
+        try {
+            FileOutputStream out = new FileOutputStream(FILE_NAME, true);
+            String newEntry = "[" + LocalDateTime.now() + "] ERROR: " + exception.getMessage() + "\n";
+            out.write(newEntry.getBytes());
+            out.close();
+        } catch (IOException e) {
+            System.out.println("Could not write to error log: " + e.getMessage());
+        }
+    }
+}
