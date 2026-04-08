481bf4745104b73862bff809f7ac57b85ac05899
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-04-07 19:49:21 -0600

    Update TradeSkill.java

--- src/main/java/edu/ucalgary/oop/TradeSkill.java
+++ src/main/java/edu/ucalgary/oop/TradeSkill.java
@@ -9,16 +9,32 @@ public class TradeSkill extends Skill {
     };
     private SkillType skillType;
 
+    /**
+     * Constructs a new TradeSkill object with the specified proficiency level and skill type
+     * 
+     * @param level The proficiency level the person has in the skill
+     * @param skillType The type of TradeSkill
+     */
     public TradeSkill(ProficiencyLevel level, SkillType skillType)
     {
         super(SkillCategory.TRADE, level);
         this.skillType = skillType;
     }
 
+    /** 
+     * Gets the skill type from the TradeSkill
+     * 
+     * @return SkillType
+     */
     public SkillType getSkillType()
     {
         return this.skillType;
     }
+    /** 
+     * Sets the skillType in the tradeskill
+     * 
+     * @param skillType The SkillType to set it to
+     */
     public void setSkillType(SkillType skillType)
     {
         this.skillType = skillType;
