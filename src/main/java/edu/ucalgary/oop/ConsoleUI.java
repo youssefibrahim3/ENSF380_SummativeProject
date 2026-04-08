899d6af5d07526251de8917a0bf32617f22e9412
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-04-07 20:09:13 -0600

    Create ConsoleUI.java

---
+++ src/main/java/edu/ucalgary/oop/ConsoleUI.java
@@ -0,0 +1,21 @@
+package edu.ucalgary.oop;
+
+import java.util.Scanner;
+
+public class ConsoleUI {
+    //private SupplyController supplyController;
+    //put the DAO calls inside of the controllers!
+    private Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Begins the console interface. 
+     * Prompts the user for their input, then goes to that interface.
+     */
+    public void start() {
+        boolean using = true;
+        while (using)
+        {
+
+        }
+    }
+}
