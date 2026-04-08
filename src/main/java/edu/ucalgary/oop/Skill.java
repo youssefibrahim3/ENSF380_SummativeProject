10b78153592138845d6cbc28a4324d51f4193fc7
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-04-07 19:55:58 -0600

    Update Skill.java

--- src/main/java/edu/ucalgary/oop/Skill.java
+++ src/main/java/edu/ucalgary/oop/Skill.java
@@ -5,25 +5,44 @@ abstract class Skill {
     private SkillCategory category;
     private ProficiencyLevel level;
 
+    /**
+     * 
+     * @param category
+     * @param level
+     */
     public Skill(SkillCategory category, ProficiencyLevel level)
     {
         this.category = category;
         this.level = level;
     }
 
+    /** 
+     * @return SkillCategory
+     */
     public SkillCategory getSkillCategory()
     {
         return this.category;
     }
+
+    /** 
+     * @param category
+     */
     public void setSkillCategory(SkillCategory category)
     {
         this.category = category;
     }
 
+    /** 
+     * @return ProficiencyLevel
+     */
     public ProficiencyLevel getProficiencyLevel()
     {
         return this.level;
     }
+
+    /** 
+     * @param level
+     */
     public void setProficiencyLevel(ProficiencyLevel level)
     {
         this.level = level;
