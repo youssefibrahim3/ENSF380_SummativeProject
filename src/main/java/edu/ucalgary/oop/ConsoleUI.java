b45bad5f2756b4edb507e7cddd2dd5c94831d787
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-04-07 20:12:46 -0600

    Update ConsoleUI.java

--- src/main/java/edu/ucalgary/oop/ConsoleUI.java
+++ src/main/java/edu/ucalgary/oop/ConsoleUI.java
@@ -13,9 +13,69 @@ public class ConsoleUI {
      */
     public void start() {
         boolean using = true;
+        System.out.println("-- Disaster Relief Management System --");
         while (using)
         {
+            System.out.println("""
+                Please enter an option:
+                0 - Exit program
+                
+                1 - Manage Victims
+                2 - Manage Supplies
+                3 - Manage Locations
+                4 - Manage Medical Records
+                5 - Manage Relationships
+                6 - Manage Inquiries
+                    """);
+            int choice = Integer.parseInt(scanner.nextLine());
+
+            //add/modify everythang
+            switch (choice) {
+                case 0:
+                    System.out.println("Exiting...\n");
+                    using = false;
+                    break;
+                case 1:
+                    manageVictims();
+                    break;
+                case 2:
+                    manageSupplies();
+                    break;
+                case 3:
+                    manageLocations();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println("Unrecognized input. Please enter a valid input.\n");
+                    break;
+            }
 
         }
+        scanner.close();
+    }
+
+    private void manageVictims()
+    {
+
+    }
+
+    private void manageSupplies()
+    {
+
+    }
+
+    private void manageLocations()
+    {
+
+    }
+
+    private void manageInquiries()
+    {
+        
     }
 }
