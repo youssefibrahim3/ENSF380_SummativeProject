a8545a6027e1fe8c0dbf25f4f42af30d8f8bd7c9
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-04-08 22:01:35 -0600

    add more tests

--- src/main/java/edu/ucalgary/oop/MedicalSkillTest.java
+++ src/main/java/edu/ucalgary/oop/MedicalSkillTest.java
@@ -1,5 +1,42 @@
 package edu.ucalgary.oop;
 
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.time.LocalDate;
+
 public class MedicalSkillTest {
-    
+
+    private MedicalSkill skill;
+
+    @Before
+    public void setUp() {
+        skill = new MedicalSkill(ProficiencyLevel.INTERMEDIATE,
+            MedicalSkill.Certification.NURSING, LocalDate.of(2027, 1, 1));
+    }
+
+    @Test
+    public void testGetCertification() {
+        assertEquals(MedicalSkill.Certification.NURSING, skill.getCertification());
+    }
+
+    @Test
+    public void testGetProficiencyLevel() {
+        assertEquals(ProficiencyLevel.INTERMEDIATE, skill.getProficiencyLevel());
+    }
+
+    @Test
+    public void testGetCertificationExpiration() {
+        assertEquals(LocalDate.of(2027, 1, 1), skill.getCertificationExpiration());
+    }
+
+    @Test
+    public void testSetCertification() {
+        skill.setCertification(MedicalSkill.Certification.DOCTOR);
+        assertEquals(MedicalSkill.Certification.DOCTOR, skill.getCertification());
+    }
+
+    @Test
+    public void testGetSkillCategory() {
+        assertEquals(SkillCategory.MEDICAL, skill.getSkillCategory());
+    }
 }
--- src/main/java/edu/ucalgary/oop/TradeSkillTest.java
+++ src/main/java/edu/ucalgary/oop/TradeSkillTest.java
@@ -1,5 +1,35 @@
 package edu.ucalgary.oop;
 
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class TradeSkillTest {
-    
+
+    private TradeSkill skill;
+
+    @Before
+    public void setUp() {
+        skill = new TradeSkill(ProficiencyLevel.BEGINNER, TradeSkill.SkillType.CARPENTRY);
+    }
+
+    @Test
+    public void testGetSkillType() {
+        assertEquals(TradeSkill.SkillType.CARPENTRY, skill.getSkillType());
+    }
+
+    @Test
+    public void testGetProficiencyLevel() {
+        assertEquals(ProficiencyLevel.BEGINNER, skill.getProficiencyLevel());
+    }
+
+    @Test
+    public void testGetSkillCategory() {
+        assertEquals(SkillCategory.TRADE, skill.getSkillCategory());
+    }
+
+    @Test
+    public void testSetSkillType() {
+        skill.setSkillType(TradeSkill.SkillType.PLUMBING);
+        assertEquals(TradeSkill.SkillType.PLUMBING, skill.getSkillType());
+    }
 }
